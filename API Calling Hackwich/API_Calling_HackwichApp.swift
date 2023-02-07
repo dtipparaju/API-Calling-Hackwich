653ff015442c5a5b802d22838afdeec691ccf2ef
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-02-07 09:31:18 -0600

    Initial Commit

---
+++ API Calling Hackwich/API_Calling_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling_HackwichApp.swift
+//  API Calling Hackwich
+//
+//  Created by Dhanush Tipparaju on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
